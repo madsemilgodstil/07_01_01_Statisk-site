7e5440e4c68a98463689640b63425bb5d2af8e6b
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-05 14:41:30 +0100

    aaa

--- productlist.js
+++ productlist.js
@@ -12,9 +12,13 @@ function showProductlist (showProduct) {
 
   myClone.querySelector('h2').textContent = showProduct.productdisplayname
   myClone.querySelector('.productId').textContent = showProduct.id
+
+  // Set the product ID and image source dynamically
+  myClone.querySelector('a').href = `produkt.html?ProductId=${showProduct.id}`
   myClone.querySelector(
     'img'
   ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${showProduct.id}.webp`
+
   myClone.querySelector('.price span').textContent = showProduct.price
   myClone.querySelector('.discount span').textContent = showProduct.discount
 
--- singleproduct.js
+++ singleproduct.js
@@ -3,13 +3,16 @@ fetch('https://kea-alt-del.dk/t7/api/products')
   .then(data => showProduct(data))
 
 function showProduct (product) {
-  console.log(product)
-  document.querySelector('.productTitle').textContent =
-    product.productdisplayname
-  document.querySelector('.productInfo').innerHTML = product.description
-  document.querySelector('.price span').textContent = product.price
-  document.querySelector('.img').src =
-    'http://assets.myntassets.com/v1/assets/banners/2015/6/26/1435317851398-23197-3chxv6.jpg'
-}
+  const urlParams = new URLSearchParams(window.location.search)
+  const id = urlParams.get('productId')
+  document.querySelector('.ProductId').textContent = id
+  console.log(id)
 
-// {"id":1163,"gender":"Men","category":"Apparel","subcategory":"Topwear","articletype":"Tshirts","basecolour":"Blue","season":"Summer","productionyear":2011,"usagetype":"Sports","productdisplayname":"Sahara Team India Fanwear Round Neck Jersey","parsed":1,"soldout":0,"relid":1163,"price":895,"discount":null,"variantname":"Roundneck Jersey","brandname":"Nike","brandbio":"Nike, creating experiences for today\u2019s athlete","brandimage":"http:\/\/assets.myntassets.com\/v1\/assets\/banners\/2015\/6\/26\/1435317851398-23197-3chxv6.jpg","agegroup":"Adults-Men","colour1":"NA","colour2":"NA","fashiontype":"Fashion","materialcaredesc":"<p>100% polyester<br>Machine wash<\/p>","sizefitdesc":null,"description":"<p>Blue round neck Sahara Team India jersey, has short sleeves, print on the chest and back chest<br><br>Warranty for manufacturing defects: 6 months (not valid on products with more than 20% discount)<br><\/p>","styledesc":"<p>Cheer for the Indian cricket team on and off the fields in this blue polo neck jersey from Nike. "}
+  console.log(product)
+  document.querySelector('h2').textContent = product.productdisplayname
+  document.querySelector('.newProductId').textContent = product.id
+  document.querySelector('.price span').textContent = product.price
+  document.querySelector(
+    '.img'
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${showProduct.id}.webp`
+}
